7d1ac7e298349cc8eb114f0662c93ad0c1f2af4c
hungpv <phanhung2702@gmail.com>
2022-07-18 17:10:37 +0700

    [Add] get dogs in home now work

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,17 +1,44 @@
 import { NavBar } from '../../components'
+import React, { useEffect, useState } from 'react'
 // import { useAuth } from '../../providers/useAuth'
 import './Home.css'
 
+const getDog = async () => {
+  return await fetch('https://dog.ceo/api/breeds/image/random')
+  .then(response => response.json())
+}
+
 export const Home = () => {
   // const { user } = useAuth()
+  const [ left, setLeft ] = useState('')
+  const [ mid, setMid ] = useState('')
+  const [ right, setRight ] = useState('')
+
+  useEffect(() => {
+    getDog().then((dog) => {
+      setLeft(dog.message)
+    })
+  }, [])
+
+  useEffect(() => {
+    getDog().then((dog) => {
+      setMid(dog.message)
+    })
+  }, [])
+
+  useEffect(() => {
+    getDog().then((dog) => {
+      setRight(dog.message)
+    })
+  }, [])
 
   return (
     <div className='home'>
       <NavBar />
       <div className='main-pic-div'>
-        <img className='left-pic' src="demo-main-pic.jpg" alt="red panda" />
-        <img className='middle-pic' src="demo-main-pic.jpg" alt="red panda" />
-        <img className='right-pic' src="demo-main-pic.jpg" alt="red panda" />
+        <img className='left-pic' src={left} alt="red panda" />
+        <img className='middle-pic' src={mid} alt="red panda" />
+        <img className='right-pic' src={right} alt="red panda" />
       </div>
     </div>
   )
