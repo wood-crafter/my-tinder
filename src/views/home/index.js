5d002da55c5cf23505f4d29419952e3adad62f40
hungpv <phanhung2702@gmail.com>
2022-07-26 14:39:45 +0700

    [Add] redirect to /login before signin

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -1,16 +1,20 @@
 import { NavBar } from '../../components'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useContext } from 'react'
+import { AuthContext } from '../../providers/use-auth'
+import { useNavigate } from 'react-router'
 import './home.css'
 
 const getDog = async () => {
   return await fetch('https://dog.ceo/api/breeds/image/random')
-  .then(response => response.json())
+    .then(response => response.json())
 }
 
 export const Home = () => {
-  const [ left, setLeft ] = useState(null)
-  const [ mid, setMid ] = useState(null)
-  const [ right, setRight ] = useState(null)
+  const { user } = useContext(AuthContext)
+  const [left, setLeft] = useState(null)
+  const [mid, setMid] = useState(null)
+  const [right, setRight] = useState(null)
+  const navigate = useNavigate()
 
   useEffect(() => {
     getDog().then((dog) => {
@@ -44,16 +48,20 @@ export const Home = () => {
     setRight(nextRight.message)
   }
 
-  return (
-    <div className='home'>
-      <NavBar />
-      <div className='main-pic-div'>
-        <img className='left-pic' src={left ?? './demo-main-pic.jpg'} alt='' />
-        <button onClick={previousClickHandler} className='previous' >&#8249;</button>
-        <img className='middle-pic' src={mid ?? './demo-main-pic.jpg'} alt='' />
-        <button onClick={nextClickHandler} className='next' >&#8250;</button>
-        <img className='right-pic' src={right ?? './demo-main-pic.jpg'} alt='' />
+  if (user) {
+    return (
+      <div className='home'>
+        <NavBar />
+        <div className='main-pic-div'>
+          <img className='left-pic' src={left ?? './demo-main-pic.jpg'} alt='' />
+          <button onClick={previousClickHandler} className='previous' >&#8249;</button>
+          <img className='middle-pic' src={mid ?? './demo-main-pic.jpg'} alt='' />
+          <button onClick={nextClickHandler} className='next' >&#8250;</button>
+          <img className='right-pic' src={right ?? './demo-main-pic.jpg'} alt='' />
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
+
+  navigate('/login', { replace: true })
 }
