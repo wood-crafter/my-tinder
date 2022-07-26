150f5a38ddc34b9470fbfa3e973d6fd63cd3478d
hungpv <phanhung2702@gmail.com>
2022-07-26 14:58:21 +0700

    [Add] redirect to /login before signin

--- src/views/about/index.js
+++ src/views/about/index.js
@@ -1,12 +1,22 @@
 import { NavBar } from '../../components'
+import React, { useContext } from 'react'
+import { AuthContext } from '../../providers/use-auth'
+import { useNavigate } from 'react-router'
 
 export const About = () => {
-  return (
-    <div>
-      <NavBar />
-      <h4>
-        About...!
-      </h4>
-    </div>
-  )
+  const { user } = useContext(AuthContext)
+  const navigate = useNavigate()
+  
+  if (user) {
+    return (
+      <div>
+        <NavBar />
+        <h4>
+          About...!
+        </h4>
+      </div>
+    )
+  }
+
+  navigate('/login', { replace: true })
 }
