dcf76c0e381ed2227e4bc94879bc65f66a337a37
hungpv <phanhung2702@gmail.com>
2022-07-29 14:21:24 +0700

    [WIP] draft reduce image loading time

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -10,30 +10,38 @@ export const Home = () => {
   const [left, setLeft] = useState(null)
   const [mid, setMid] = useState(null)
   const [right, setRight] = useState(null)
+  const [randomDog1, setRandomDog1] = useState(null)
+  const [randomDog2, setRandomDog2] = useState(null)
 
   useEffect(() => {
-    Promise.all([getDog(), getDog(), getDog()])
+    Promise.all([getDog(), getDog(), getDog(), getDog(), getDog()])
       .then(dogs => {
-        const [leftDog, midDog, rightDog] = dogs.map(dog => dog.message)
-  
+        const [leftDog, midDog, rightDog, randomDog1, randomDog2] = dogs.map(dog => dog.message)
+
         setLeft(leftDog)
         setMid(midDog)
         setRight(rightDog)
+        setRandomDog1(randomDog1)
+        setRandomDog2(randomDog2)
       })
   }, [])
 
   const nextClickHandler = async () => {
     setRight(mid)
     setMid(left)
-    const nextLeft = await getDog()
-    setLeft(nextLeft.message)
+    setLeft(randomDog1)
+    setRandomDog1(randomDog2)
+    const nextRandomDog = await getDog()
+    setRandomDog2(nextRandomDog.message)
   }
 
   const previousClickHandler = async () => {
     setLeft(mid)
     setMid(right)
-    const nextRight = await getDog()
-    setRight(nextRight.message)
+    setRight(randomDog1)
+    setRandomDog1(randomDog2)
+    const nextRandomDog = await getDog()
+    setRandomDog2(nextRandomDog.message)
   }
 
   if (!user) return <Navigate to="/login" />
@@ -41,6 +49,8 @@ export const Home = () => {
   return (
     <div className='home'>
       <NavBar />
+      <img className='random-image' src={randomDog1 ?? ''} alt=''></img>
+      <img className='random-image' src={randomDog2 ?? ''} alt=''></img>
       <div className='main-pic-div'>
         <img className='left-pic' src={left ?? './demo-main-pic.jpg'} alt='' />
         <button onClick={previousClickHandler} className='previous' >&#8249;</button>
