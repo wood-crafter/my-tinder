d483115e00e661d0aaecca70e997b0280b36157f
hungpv <phanhung2702@gmail.com>
2022-08-13 09:25:07 +0700

    [Add] user page draft

--- src/views/users/index.js
+++ src/views/users/index.js
@@ -1,12 +1,26 @@
 import { NavBar } from '../../components'
+import './users.css'
 
 export const Users = () => {
+  const dummyUserInfo = {
+    name: 'dummyName',
+    contact: 'dummyContact',
+    info: 'dummyInfo',
+    bio: 'dummyBio',
+    type: 'dummyType'
+  }
+
+  // const dummyAlbum
+
   return (
-    <div>
+    <div className='user'>
       <NavBar />
-      <h4>
-        Users...!
-      </h4>
+      <div className='name'>{dummyUserInfo.name}</div>
+      <div className='contact'>{dummyUserInfo.contact}</div>
+      <div className='info'>{dummyUserInfo.info}</div>
+      <div className='bio'>{dummyUserInfo.bio}</div>
+      <div className='type'>{dummyUserInfo.type}</div>
+
     </div>
   )
 }
