7a2d18c2278337b1d25e0d4a620d4080593f2e95
hungpv <phanhung2702@gmail.com>
2022-08-30 14:44:39 +0700

    [Add] user information detail scraffold

--- src/views/users/index.js
+++ src/views/users/index.js
@@ -4,10 +4,17 @@ import './users.css'
 export const Users = () => {
   const dummyUserInfo = {
     name: 'dummyName',
-    contact: 'dummyContact',
-    info: 'dummyInfo',
+    info: {
+      email: 'phanhung2702@gmail.com',
+      phone: '0366513328',
+      address: 'HN',
+      facebook: 'dummyFacebookLink',
+      instagram: 'dummyInstagramLink'
+    },
     bio: 'dummyBio',
-    type: 'dummyType'
+    type: 'dummyType',
+    interest: 'dummyInterest',
+    jobTitle: 'dev',
   }
 
   // const dummyAlbum
@@ -21,16 +28,21 @@ export const Users = () => {
             <div className='avatar-container'>
               <img src='https://avatars.githubusercontent.com/u/59172319?v=4' className='avatar'></img>
             </div>
-            <div>Display name</div>
-            <div>Bio</div>
+            <div>{dummyUserInfo.name}</div>
+            <div>{dummyUserInfo.bio}</div>
+            <div>{dummyUserInfo.interest}</div>
+            <div>{dummyUserInfo.jobTitle}</div>
           </div>
 
           <div className='master-infor'>
-            <div id='name' >{dummyUserInfo.name}</div>
-            <div id='contact' >{dummyUserInfo.contact}</div>
-            <div id='info' >{dummyUserInfo.info}</div>
-            <div id='bio' >{dummyUserInfo.bio}</div>
-            <div id='type' >{dummyUserInfo.type}</div>
+            <form className='user-profile-form-update'>
+              <div id='info' className='master-detail' ><label htmlFor='user-profile-email' >Email: </label><input id='user-profile-email' placeholder={dummyUserInfo.info.email}></input></div>
+              <div id='phone' className='master-detail' ><label htmlFor='user-profile-phone' >Phone: </label><input id='user-profile-phone' placeholder={dummyUserInfo.info.phone}></input></div>
+              <div id='address' className='master-detail' ><label htmlFor='user-profile-address' >Address: </label><input id='user-profile-address' placeholder={dummyUserInfo.info.address}></input></div>
+              <div id='facebook' className='master-detail' ><label htmlFor='user-profile-facebook' >Facebook: </label><input id='user-profile-facebook' placeholder={dummyUserInfo.info.facebook}></input></div>
+              <div id='instagram' className='master-detail' ><label htmlFor='user-profile-instagram' >Instagram: </label><input id='user-profile-instagram' placeholder={dummyUserInfo.info.instagram}></input></div>
+              <div id='type' className='master-detail' >{dummyUserInfo.type}</div>
+            </form>
           </div>
         </div>
       </div>
