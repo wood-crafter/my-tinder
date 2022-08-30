3a575a9eac2278d2171b775980742b7c6ca27e43
hungpv <phanhung2702@gmail.com>
2022-08-30 15:57:50 +0700

    [add] binding data to input

--- src/views/users/index.js
+++ src/views/users/index.js
@@ -1,7 +1,18 @@
+import { useState } from 'react'
 import { NavBar } from '../../components'
 import './users.css'
 
 export const Users = () => {
+  const [name, setName] = useState('')
+  const [bio, setBio] = useState('')
+  const [interest, setInterest] = useState('')
+  const [jobTitle, setJobTitle] = useState('')
+  const [email, setEmail] = useState('')
+  const [phone, setPhone] = useState('')
+  const [address, setAddress] = useState('')
+  const [facebook, setFacebook] = useState('')
+  const [instagram, setInstagram] = useState('')
+
   const dummyUserInfo = {
     name: 'dummyName',
     info: {
@@ -17,7 +28,10 @@ export const Users = () => {
     jobTitle: 'dev',
   }
 
-  // const dummyAlbum
+  const handleUpdateMasterInfo = (event) => {
+    event.preventDefault();
+    console.info(email)
+  }
 
   return (
     <div className='user' >
@@ -28,21 +42,22 @@ export const Users = () => {
             <div className='avatar-container'>
               <img src='https://avatars.githubusercontent.com/u/59172319?v=4' className='avatar'></img>
             </div>
-            <div><label htmlFor='user-profile-name' >Name: </label><input id='user-profile-name' placeholder={dummyUserInfo.name}></input></div>
-            <div><label htmlFor='user-profile-bio' >Bio: </label><input id='user-profile-bio' placeholder={dummyUserInfo.bio}></input></div>
-            <div><label htmlFor='user-profile-interest' >Interest: </label><input id='user-profile-interest' placeholder={dummyUserInfo.interest}></input></div>
-            <div><label htmlFor='user-profile-jobTitle' >Job title: </label><input id='user-profile-jobTitle' placeholder={dummyUserInfo.jobTitle}></input></div>
+            <div><label htmlFor='user-profile-name' >Name: </label><input id='user-profile-name' placeholder={dummyUserInfo.name} value={name} onChange={e => setName(e.target.value)} ></input></div>
+            <div><label htmlFor='user-profile-bio' >Bio: </label><input id='user-profile-bio' placeholder={dummyUserInfo.bio} value={bio} onChange={e => setBio(e.target.value)} ></input></div>
+            <div><label htmlFor='user-profile-interest' >Interest: </label><input id='user-profile-interest' placeholder={dummyUserInfo.interest} value={interest} onChange={e => setInterest(e.target.value)} ></input></div>
+            <div><label htmlFor='user-profile-jobTitle' >Job title: </label><input id='user-profile-jobTitle' placeholder={dummyUserInfo.jobTitle} value={jobTitle} onChange={e => setJobTitle(e.target.value)} ></input></div>
           </div>
 
           <div className='master-infor'>
-            <form className='user-profile-form-update'>
-              <div id='info' className='master-detail' ><label htmlFor='user-profile-email' >Email: </label><input id='user-profile-email' placeholder={dummyUserInfo.info.email}></input></div>
-              <div id='phone' className='master-detail' ><label htmlFor='user-profile-phone' >Phone: </label><input id='user-profile-phone' placeholder={dummyUserInfo.info.phone}></input></div>
-              <div id='address' className='master-detail' ><label htmlFor='user-profile-address' >Address: </label><input id='user-profile-address' placeholder={dummyUserInfo.info.address}></input></div>
-              <div id='facebook' className='master-detail' ><label htmlFor='user-profile-facebook' >Facebook: </label><input id='user-profile-facebook' placeholder={dummyUserInfo.info.facebook}></input></div>
-              <div id='instagram' className='master-detail' ><label htmlFor='user-profile-instagram' >Instagram: </label><input id='user-profile-instagram' placeholder={dummyUserInfo.info.instagram}></input></div>
-              <div id='type' className='master-detail' >{dummyUserInfo.type}</div>
+            <form className='user-profile-form-update' onSubmit={handleUpdateMasterInfo} >
+              <div id='info' className='master-detail' ><label htmlFor='user-profile-email' >Email: </label><input id='user-profile-email' placeholder={dummyUserInfo.info.email} value={email} onChange={e => setEmail(e.target.value)} ></input></div>
+              <div id='phone' className='master-detail' ><label htmlFor='user-profile-phone' >Phone: </label><input id='user-profile-phone' placeholder={dummyUserInfo.info.phone} value={phone} onChange={e => setPhone(e.target.value)} ></input></div>
+              <div id='address' className='master-detail' ><label htmlFor='user-profile-address' >Address: </label><input id='user-profile-address' placeholder={dummyUserInfo.info.address} value={address} onChange={e => setAddress(e.target.value)} ></input></div>
+              <div id='facebook' className='master-detail' ><label htmlFor='user-profile-facebook' >Facebook: </label><input id='user-profile-facebook' placeholder={dummyUserInfo.info.facebook} value={facebook} onChange={e => setFacebook(e.target.value)} ></input></div>
+              <div id='instagram' className='master-detail' ><label htmlFor='user-profile-instagram' >Instagram: </label><input id='user-profile-instagram' placeholder={dummyUserInfo.info.instagram} value={instagram} onChange={e => setInstagram(e.target.value)} ></input></div>
+              <button type='submit' >Change</button>
             </form>
+            <div id='type' className='master-detail' >{dummyUserInfo.type}</div>
           </div>
         </div>
       </div>
