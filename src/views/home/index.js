d1c6dfddfd0f325f64bd4cb206533641d544d7ee
hungpv <phanhung2702@gmail.com>
2022-09-26 11:34:40 +0700

    Merge branch 'master' into 33-prevent-loading-dog-image-when-image-change

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -9,42 +9,6 @@ import './home.css'
 
 export const Home = () => {
   const { user } = useContext(AuthContext)
-  const [left, setLeft] = useState(null)
-  const [mid, setMid] = useState(null)
-  const [right, setRight] = useState(null)
-  const dogs = useInitDog()
-
-  useEffect(() => {
-    if (!dogs.length) return
-
-    setLeft(dogs[0])
-    setMid(dogs[1])
-    setRight(dogs[2])
-  }, [dogs])
-
-  const handleNextClick = async () => {
-    setRight(mid)
-    setMid(left)
-    const nextLeft = await getDog()
-    setLeft(nextLeft.message)
-  }
-
-  const handlePreviousClick = async () => {
-    setLeft(mid)
-    setMid(right)
-    const nextRight = await getDog()
-    setRight(nextRight.message)
-  }
-
-  const handleArrowKeyDown = async (e) => {
-    if (e.key === 'ArrowLeft') {
-      await handlePreviousClick()
-    }
-
-    if (e.key === 'ArrowRight') {
-      await handleNextClick()
-    }
-  }
 
   // FIXME: This guard return could be turned in to a `hook`, or a `HoC`...!
   if (!user) return <Navigate to="/login" />
